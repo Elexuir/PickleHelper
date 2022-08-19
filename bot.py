b779cbfd3aa8a24f54e393cfa08384c910970f54
Xander <xanderjdavis@icloud.com>
2022-08-19 17:32:33 -0400

    Initial-Commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.environ.get("SECRET_TOKEN")
+
+@bot.event
+async def on_ready():
+    print("Logged into bot 'The-Pickle-Helper'!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
